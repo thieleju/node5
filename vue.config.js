96579398babcd0e3a8e7ae418f9aad82f6e1e9de
Julian T <thiele.juli@gmail.com>
2020-09-21 22:47:56 +0200

    switched back to http sad life is sad

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    https: {
-      key: fs.readFileSync("cert/privkey.pem"),
-      cert: fs.readFileSync("cert/fullchain.pem")
-    }
+    // https: {
+    //   key: fs.readFileSync("cert/privkey.pem"),
+    //   cert: fs.readFileSync("cert/fullchain.pem")
+    // }
   }
 }
